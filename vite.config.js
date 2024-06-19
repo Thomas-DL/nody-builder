09633d55f05601a2d3f29688ebe4015c1bb8a299
Thomas <software@thomasdelage.com>
2024-06-19 15:59:20 +0200

    preline plugin fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         rollupOptions: {
-            external: ['preline']
+            external: ['preline', 'preline/plugin']
             }
         },
     plugins: [
